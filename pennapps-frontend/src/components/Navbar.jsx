3585cf2f312049d0856df450c323f6efc1164a7f
Jessica Ye <ssicaj00@gmail.com>
2023-09-10 02:01:55 -0400

    Edited menu css

--- pennapps-frontend/src/components/Navbar.jsx
+++ pennapps-frontend/src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import{BrowserRouter,Route,Link} from "react-router-dom";
 import "../css/Navbar.css";
+import "../css/Home.css";
 
 
 function Navbar(){
     return(
     <div class="navbar">
+
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+        </style>
         
         <div class="menubutton">
-            <nav class="nav">
-                <ul>
+            <nav class="navMenu">
+                <ul class = "navUL">
                     <li>
                         <a href="/">Home</a>
                     </li>
--- pennapps-frontend/src/pages/Home.jsx
+++ pennapps-frontend/src/pages/Home.jsx
@@ -4,10 +4,18 @@ import "../css/Home.css";
 function Home(){
     return(
         <body>
+
+            <style>
+            @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+            </style>
             <div class="introduction">
                 <h1>SnowPast</h1>
                 <h4>this is our catchphrase</h4>
                 <button type="button" id="try">Try It</button>
+            </div>
+
+            <div>
+                <h2>How can we forecast and help others mitigate the effects of less or more snowpack?</h2>
             </div>
 
             <div>
