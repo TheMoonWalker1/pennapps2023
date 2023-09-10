bfb2bc27eb78e12907045db9d21342302748ed9a
Chinmay Bhandaru <praneethsbhandaru@gmail.com>
2023-09-10 02:35:02 -0400

    feat added realtime functionality

--- backend/realtime.py
+++ backend/realtime.py
@@ -1,43 +1,74 @@
 import ee
 
-# Authenticate to Earth Engine using your credentials (if required)
-# ee.Authenticate()
-
 # Initialize the Earth Engine API
 ee.Initialize()
 
-# Define the ImageCollection
-collection = ee.ImageCollection("IDAHO_EPSCOR/GRIDMET")
-
 # Input latitude and longitude from the user
-latitude = float(input("Enter Latitude: "))
-longitude = float(input("Enter Longitude: "))
+latitude = float(input("Enter latitude: "))
+longitude = float(input("Enter longitude: "))
 
-# Create a point geometry for the user-specified location
+# Create a point geometry using the user-provided coordinates
 point = ee.Geometry.Point(longitude, latitude)
 
-# Filter the collection based on time and location
-filtered_collection = collection.filterBounds(point).sort('system:time_start', False)
+# Define the ImageCollection to fetch data from
+image_collection = ee.ImageCollection("IDAHO_EPSCOR/GRIDMET")
 
-# Get the latest image from the filtered collection
-latest_image = filtered_collection.first()
+# Filter the ImageCollection by location
+filtered_collection = image_collection.filterBounds(point)
 
-# Print the latest image information
-print("Latest Image ID:", latest_image.id())
-print("Date of Latest Image:", ee.Date(latest_image.get('system:time_start')).format('YYYY-MM-dd').getInfo())
+# Sort the collection by time in descending order to get the latest data
+sorted_collection = filtered_collection.sort("system:time_start", False)
 
-# Optionally, you can visualize the image using the Map
-# Uncomment the following lines if you have the ipygee library installed
-# import ipygee as ui
-# Map = ui.Map()
-# Map.centerObject(point, 6)
-# Map.addLayer(latest_image, {}, "Latest Image")
-# Map
+# Get the latest image in the collection
+latest_image = sorted_collection.first()
 
-# You can also export the image if needed
-# Export.image.toDrive({
-#     image: latest_image,
-#     description: 'latest_image',
-#     folder: 'GEE_outputs',
-#     scale: 1000,  # Set the scale according to your requirements
-# })
+# Select the desired bands and properties
+data = latest_image.select(['eto', 'pr', 'rmax', 'rmin', 'tmmn', 'tmmx', 'vpd', 'vs'])
+
+# Get the image properties as a dictionary
+properties = latest_image.getInfo()
+
+# Extract the timestamp from the image properties
+timestamp = properties['properties']['system:time_start']
+
+# Convert the timestamp to a human-readable format
+from datetime import datetime
+timestamp = datetime.utcfromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+
+# Print the data and timestamp
+print("Data at latitude {} and longitude {} on {}: {}".format(latitude, longitude, timestamp, data.getInfo()))
+# Extract numeric values from the data dictionary
+numeric_values = {band['id']: latest_image.select(band['id']).reduceRegion(
+    reducer=ee.Reducer.mean(),
+    geometry=point,
+    scale=30  # Adjust the scale as needed
+).get(band['id']).getInfo() for band in dict(data.getInfo())['bands']}
+
+import pickle
+def load_xgboost_model(model_file):
+    with open(model_file, 'rb') as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
+
+loaded_xgb_model = load_xgboost_model('xgb_model.pkl')
+
+user_input = {
+    'm': float(datetime.utcfromtimestamp(properties['properties']['system:time_start'] / 1000).strftime('%m')),
+    'day': float(datetime.utcfromtimestamp(properties['properties']['system:time_start'] / 1000).strftime('%d'))
+}
+# Print the numeric values
+for band, value in numeric_values.items():
+    print(f"{band}: {value}")
+    user_input[band] = float(value)
+
+user_input['lat'] = latitude
+user_input['lon'] = longitude
+
+import pandas as pd
+user_input_df = pd.DataFrame([user_input])
+
+# Make predictions using the loaded model and user input
+predictions = loaded_xgb_model.predict(user_input_df)
+
+# 'predictions' will contain the predicted values
+print(f"Predicted value: {predictions[0]}")
