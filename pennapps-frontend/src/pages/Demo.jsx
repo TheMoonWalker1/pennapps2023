bd5d9c79437a54cd0d01d612c8e40023e80cd85f
Chinmay Bhandaru <praneethsbhandaru@gmail.com>
2023-09-10 06:04:12 -0400

    fix: react functionality w/ forms

--- pennapps-frontend/src/pages/Demo.jsx
+++ pennapps-frontend/src/pages/Demo.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function Demo(){
-    return(
-        <body>
-            <p>The application requires the identification of </p>
-            <form>
-                <label for="longitude">Longitude
-                    <input type="text" id="longitude" name="longitude"/>
-                </label>
-                
-                <label for="Latitude">Latitude
-                    <input type="text" id="latitude" name="latitude"/>
-                </label>
-                
-                <input type="submit" value="Submit"/>
-            </form>
-        </body> 
-        
-        
-    );
+function MyComponent() {
+  const [latitude, setLatitude] = useState(0); // Initialize with default values
+  const [longitude, setLongitude] = useState(0);
+
+  const sendRequest = () => {
+    // Fetch the CSRF token from the Django server by making a GET request
+    axios.get('/csrf/').then((response) => {
+      // Extract the CSRF token from the response
+      const csrfToken = response.data.csrfToken;
+
+      // Include the CSRF token in the request headers
+      axios
+        .post(
+          '/ajax/',
+          {
+            latitude: latitude,
+            longitude: longitude,
+          },
+          {
+            headers: {
+              'X-CSRFToken': csrfToken,
+            },
+          }
+        )
+        .then((response) => {
+          // Handle the response from Django here
+          console.log(response.data);
+        })
+        .catch((error) => {
+          // Handle any errors here
+          console.error(error);
+        });
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Latitude"
+        onChange={(e) => setLatitude(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Longitude"
+        onChange={(e) => setLongitude(e.target.value)}
+      />
+      <button onClick={sendRequest}>Send Request</button>
+    </div>
+  );
 }
 
-export default Demo;
+export default MyComponent;
